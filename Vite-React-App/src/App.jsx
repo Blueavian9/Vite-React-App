46da8479f5e20c5c3b9666984a3a73ec63de05bc
Cesar Aguilar <Blueavian9@gmail.com>
2025-01-17 15:44:09 -0800

    updated App.jsx

--- Vite-React-App/src/App.jsx
+++ Vite-React-App/src/App.jsx
@@ -42,14 +42,22 @@ const App = () => {
       {/* Header Section */}
       <header className="header">
         <div className="logo">
-          <a href="/">MyBrand</a>
+          <a href="/">Data Storage Comparator App</a>
         </div>
         <nav>
           <ul className="nav-links">
-            <li><a href="#home">Home</a></li>
-            <li><a href="#about">About</a></li>
-            <li><a href="#features">Features</a></li>
-            <li><a href="#contact">Contact</a></li>
+            <li>
+              <a href="#home">Home</a>
+            </li>
+            <li>
+              <a href="#about">About</a>
+            </li>
+            <li>
+              <a href="#features">Features</a>
+            </li>
+            <li>
+              <a href="#contact">Contact</a>
+            </li>
           </ul>
         </nav>
       </header>
